dadd8ab7e7a164862d95612482d7dfe6d11339d8
Ahmed Abou Zaidi <ahmed.abouzaidi@inetum-realdolmen.world>
2023-09-07 16:33:55 +0200

    Create Notes controller

---
+++ AccessBlobStorage/Controllers/NotesController.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AccessBlobStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public NotesController(BlobServiceClient blobServiceClient)
+        {
+            this._blobServiceClient = blobServiceClient;
+        }
+
+        [HttpPost("{blobName}")]
+        public async Task AddLine(string blobName, [FromQuery] string line)
+        {
+            var containerClient = _blobServiceClient.GetBlobContainerClient("notes");
+            await containerClient.CreateIfNotExistsAsync();
+
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            var oldContent = await ReadWholeContent(blobClient);
+            var newContent = oldContent + '\n' + line;
+            await WriteWholeContent(blobClient, newContent);
+        }
+
+        private async Task<string> ReadWholeContent(BlobClient blobClient)
+        {
+            using var read = await blobClient.OpenReadAsync();
+            using var streamReader = new StreamReader(read);
+            return await streamReader.ReadToEndAsync();
+        }
+
+        private async Task WriteWholeContent(BlobClient blobClient, string content)
+        {
+            using var writer = await blobClient.OpenWriteAsync(true);
+            using var streamWriter = new StreamWriter(writer);
+            await streamWriter.WriteAsync(content);
+        }
+    }
+}
