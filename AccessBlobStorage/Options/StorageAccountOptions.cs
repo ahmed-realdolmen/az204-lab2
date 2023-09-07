c6225759bdbdce5d6af6a1e881a326f9da286f69
Ahmed Abou Zaidi <ahmed.abouzaidi@inetum-realdolmen.world>
2023-09-07 17:37:46 +0200

    Inject blob service client

---
+++ AccessBlobStorage/Options/StorageAccountOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace AccessBlobStorage.Options
+{
+    public class StorageAccountOptions
+    {
+        public string ConnectionString { get; set; }
+    }
+}
--- AccessBlobStorage/Program.cs
+++ AccessBlobStorage/Program.cs
@@ -1,3 +1,7 @@
+using AccessBlobStorage.Options;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var config = builder.Configuration.GetSection("StorageAccount").Get<StorageAccountOptions>();
+builder.Services.AddScoped(_ =>
+{
+    return new BlobServiceClient(config.ConnectionString);
+});
 
 var app = builder.Build();
 
